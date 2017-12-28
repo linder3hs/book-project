b9d046e2857b10303b96cbedcee1d36fafe9a5a8
Linder Hassinger <linderhassinger00@gmail.com>
2017-12-28 12:18:31 -0500

    js en orden

--- public/js/book.js
+++ public/js/book.js
@@ -1,1 +1,51 @@
+// Ajax para buscar libros por isbn
+var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:9780545010221";
+$(document).ready(function () {
 
+    // Capturamos el valor del input y traemos los datos en json
+    $('.btnBuscar').on('click', function () {
+        var isbn = $('#numberIsbn').val();
+        if (isbn != ""){
+            $('#numberIsbn').val("");
+            $.ajax({
+                type: 'GET',
+                url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:'+isbn,
+                data: { get_param: 'value' },
+                dataType: 'json',
+                success: function (data) {
+                    if (data == 'success') {
+                        var img = data['items']['0']['volumeInfo']['imageLinks']['smallThumbnail'];
+                        console.log("foto: " + data['items'][0]['volumeInfo']['imageLinks']['smallThumbnail']);
+                        $('#content').html('<h5>Titulo del libro: ' + data['items'][0]['volumeInfo']['title'] + '</h5>' + '<p>Autor:  ' + data['items'][0]['volumeInfo']['authors'] + '</p>' + '<p>Año de publicación:  ' + data['items'][0]['volumeInfo']['publishedDate'] + '</p>' + '<br>');
+                        $('#image').attr('src', img);
+                        $('#btnRe').show();
+                        $('#btnCancel').show();
+                        $('.btnBuscar').hide();
+                    } else {
+                        bookNoFound();
+                    }
+                }
+            });
+        } else {
+            alert("Debe rellenar los campos o ingresar valores validos");
+        }
+    });
+
+    $('#btnCancel').on('click', function () {
+        $('#btnRe').slideUp();
+        $('#btnCancel').slideUp();
+        $('.btnBuscar').slideDown();
+        $('#content').html("");
+        $('#image').attr('src', "");
+    });
+
+    function bookNoFound() {
+        $('#contenedorIsbn').slideUp();
+        $('.sinIsbn').slideDown();
+    }
+
+    $('#cancelSinIsbn').on('click',function () {
+        $('#contenedorIsbn').slideDown();
+        $('.sinIsbn').slideUp();
+    });
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="/js/book.js"></script>
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
