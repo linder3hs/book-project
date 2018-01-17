816dbdf511644bdde1997f347284b014a4d157d8
Linder Hassinger <linderhassinger00@gmail.com>
2018-01-17 16:15:09 -0500

    momentaneo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -36,4 +37,58 @@
         </div>
     </div>
 </div>
+    <script>
+        // Ajax para buscar libros por isbn
+
+        // Capturamos el valor del input y traemos los datos en json
+        $('.btnBuscar').on('click', function () {
+            var isbn = $('#numberIsbn').val();
+            if (isbn != ""){
+                $('#numberIsbn').val("");
+                $.ajax({
+                    type: 'GET',
+                    url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:'+isbn,
+                    data: { get_param: 'value' },
+                    dataType: 'json',
+                    success: function (data) {
+                        console.log(data['totalItems']);
+                        if (data['totalItems'] == 1) {
+                            var img = data['items']['0']['volumeInfo']['imageLinks']['smallThumbnail'];
+                            console.log("foto: " + data['items'][0]['volumeInfo']['imageLinks']['smallThumbnail']);
+                            $('#content').html('<h5>Titulo del libro: ' + data['items'][0]['volumeInfo']['title'] + '</h5>' + '<p>Autor:  ' + data['items'][0]['volumeInfo']['authors'] + '</p>' + '<p>Año de publicación:  ' + data['items'][0]['volumeInfo']['publishedDate'] + '</p>' + '<br>');
+                            $('#image').attr('src', data['items']['0']['volumeInfo']['imageLinks']['smallThumbnail']);
+                            $('#btnRe').show();
+                            $('#btnCancel').show();
+                            $('.btnBuscar').hide();
+                        } else {
+                            bookNoFound();
+                        }
+                    }
+                });
+            } else {
+                alert("Debe rellenar los campos o ingresar valores validos");
+            }
+        });
+
+        $('#btnCancel').on('click', function () {
+            $('#btnRe').slideUp();
+            $('#btnCancel').slideUp();
+            $('.btnBuscar').slideDown();
+            $('#content').html("");
+            $('#image').attr('src', "");
+        });
+
+        function bookNoFound() {
+            $('#contenedorIsbn').slideUp();
+            $('.sinIsbn').slideDown();
+        }
+
+        $('#cancelSinIsbn').on('click',function () {
+            $('#contenedorIsbn').slideDown();
+            $('.sinIsbn').slideUp();
+        });
+
+
+
+    </script>
 @endsection
