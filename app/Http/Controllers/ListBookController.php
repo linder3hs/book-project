51d4aef5bb64a7d0a5c50893844dfb981876ed99
Linder Hassinger <linderhassinger00@gmail.com>
2018-01-25 23:43:07 -0500

    Book Controller

---
+++ app/Http/Controllers/ListBookController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\book_register;
+use Illuminate\Http\Request;
+
+class ListBookController extends Controller
+{
+    public function index() {
+        $books = book_register::all();
+        $list = array('libros' => $books);
+        return view('list_book', $list);
+    }
+}
