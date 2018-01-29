ceb823d808ac152edf634a8a077408a74ccccfae
Linder Hassinger <linderhassinger00@gmail.com>
2018-01-28 19:36:07 -0500

    profile and section of my books

--- resources/views/list_book.blade.php
+++ resources/views/list_book.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.app')
 @section('content')
+    <br><br>
     <div class="container">
     <h3>Lista de mis libros</h3>
     @foreach($libros as $libro)
         @if( $libro->user_id == Auth::user()->id)
-            <div class="form-group" style="border: solid 1px #ccc; background-color: #fff; border-radius: 8px;">
-                <div style="padding: 8px;">
-                    <h4>Titulo: {{ $libro->title }}</h4>
-                    <p>Autor: {{ $libro->author }}</p>
-                    <p><span class="glyphicon glyphicon-calendar">&nbsp;</span>Año de publicación: &nbsp;{{ substr($libro->public_date,0) }}</p>
-                    @if(strpos($libro->image, 'http') !== false)
-                    <img src="{{ $libro->image }}" class="image-responsive" alt="" style="width: 100px; height: 100px">
-                    @else
-                    <img src="/images/{{ $libro->image }}" class="image-responsive" alt="" style="width: 100px; height: 100px">
-                    @endif
+                <div class="list-group">
+                    <a class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">{{ $libro->title }}</h5>
+                        </div>
+                        <span>Autor: {{ $libro->author }}</span><br>
+                        <span>{{ substr($libro->public_date,0) }}</span><br>
+                        <div class="d-flex w-100 justify-content-between">
+                            @if(strpos($libro->image, 'http') !== false)
+                                <img src="{{ $libro->image }}" class="img-fluid rounded">
+                            @else
+                                <img src="/images/{{ $libro->image }}" class="img-fluid rounded" style="height: 130px; width:auto;">
+                            @endif
+                        </div>
+                    </a>
                 </div>
-            </div>
+                <br>
+
         @endif
     @endforeach
     </div>
