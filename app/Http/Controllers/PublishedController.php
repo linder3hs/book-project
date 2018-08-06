639f3b85e4bb232e028426f20017942df53a2567
linderhassinger <linderhassinger00@gmail.com>
2018-08-05 20:45:00 -0500

    join con user

--- app/Http/Controllers/PublishedController.php
+++ app/Http/Controllers/PublishedController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers;
 use App\Publicaciones;
 use Illuminate\Http\Request;
 use App\Comemnts;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class PublishedController extends Controller
 {
     public function index() {
         $publics = Publicaciones::orderBy('id', 'desc')->get();
-        $comentarios = Comemnts::all();
-        return view('published', compact('publics', 'comentarios'));
+        $comentarios = DB::table('comemnts')->
+        join('users', 'users.id', '=', 'comemnts.user_id')->
+        orderBy('comemnts.id', 'desc')->get();
+        return view('published')->with('publics', $publics)->with('comentarios', $comentarios);
     }
 
     public function storage(Request $request) {
@@ -25,7 +29,9 @@ class PublishedController extends Controller
     }
 
     public function comment(Request $request) {
+        $user = $request->user();
         $comentario = new Comemnts();
+        $comentario->user_id = $user->id;
         $comentario->comentario = $request->input('comentario');
         $comentario->pub_id = $request->input('idpu');
         $comentario->save();
--- resources/views/published.blade.php
+++ resources/views/published.blade.php
@@ -16,6 +16,9 @@
             margin-right:25px;
             margin-left: 10px;
         }
+        textarea:focus {
+            outline: 0 none;
+        }
     </style>
     <div id="padre" class="d-none d-lg-block">
         <div id="left" class="card">
@@ -30,11 +33,15 @@
     </div>
     <div class="container col-lg-5 col-md-offset-5">
         <div class="form-group">
-            <h5>Tús publicaciones</h5>
             <form action="{{ url('/home/publicaciones') }}" method="post">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <textarea type="text" name="public" class="form-control" style="height: 150px;"></textarea><br>
-                <input type="submit" value="Publicar" class="btn btn-success">
+                <div class="card">
+                    <textarea placeholder="Comparte tus conecimeintos" type="text" name="public" class="form-control" style="height: 100px; margin-bottom: 0px; resize: none; border: 0; outline: none; box-shadow: none;"></textarea><br>
+                    <div align="right" style="margin-top: 0px;">
+                        <hr>
+                        <input type="submit" value="Publicar" style="background-color: #117ab1; color: white; margin-top: 0px; margin-bottom: 10px; margin-right: 10px;" class="btn btn-sm">
+                    </div>
+                </div>
             </form>
         </div>
     </div>
@@ -46,27 +53,31 @@
                         <h5 class="mb-1">{{ $public->publicaciones }}</h5>
                         <small>Publicado por {{ $public->name }}</small>
                     </div>
-                    <small>Hace {{ $public->created_at }}</small>
+                    <small><span class="fa fa-calendar-times-o"></span> Desde el {{ date('d-m-Y', strtotime($public->created_at)) }}</small>
                     <br>
-                <small>Comentarios</small>
                 </a>
+                <li class="list-group-item">
+                    <form action="{{ url('/home/comment') }}" method="post">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <input type="hidden" name="idpu" value="{{ $public->id }}">
+                        <div class="input-group mb-3">
+                            <input type="text" style="outline: none; box-shadow: none;" name="comentario" class="form-control" placeholder="Escribe un comentario..." aria-label="Recipient's username" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="submit"><span class="fa fa-send"></span></button>
+                            </div>
+                        </div>
+                    </form>
+                </li>
                 @foreach($comentarios as $comentario)
                    @if( $comentario->pub_id == $public->id)
-                    <ul class="list-group-item">
-                    {{ $comentario->comentario }}
+                    <ul class="list-group-item" style="background-color: #f3f6f8;">
+                        <span class="text-dark font-weight-bold">{{ $comentario->name . " " . $comentario->apellido }}</span>
+                        <span style="float: right; font-size: 12px;">{{ date('d-m-Y', strtotime($comentario->created_at)) }}</span>
+                        <br>
+                        <span style="font-size: 14px; font-family: Helvetica; ">{{ $comentario->comentario }}</span>
                     </ul>
                   @endif
                 @endforeach
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <form action="{{ url('/home/comment') }}" method="post">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <input type="hidden" name="idpu" value="{{ $public->id }}">
-                            <input type="text" name="comentario" class="form-control" style="height: 35px;">
-                            <button type="submit" class="btn btn-sm btn-success"><span class="icon-pencil"></span></button>
-                        </form>
-                    </li>
-                </ul>
             </div>
             <br>
         @endforeach
