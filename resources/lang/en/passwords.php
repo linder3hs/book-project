841816d6bf54d93859714e033bc0f01d9ac21a03
linder3hs <linderhassinger00@gmail.com>
2018-11-03 17:58:40 -0500

    fix

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -15,7 +15,7 @@ return [
 
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'sent' => 'Se le envió un link a su correo para que pueda recuperar su contraseña!',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
 
